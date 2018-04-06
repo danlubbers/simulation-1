51b619400d6a9434982004584a6b39e445639d04
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-06 11:15:40 -0600

    minor changes

--- shelfie/src/App.js
+++ shelfie/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Component/Dashboard/Dashboard.js';
 import Form from './Component/Form/Form.js';
-import Product from './Component/Product/Product.js';
 import Header from './Component/Header/Header.js';
 
 
